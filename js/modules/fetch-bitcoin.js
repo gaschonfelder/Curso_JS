14307f694dea8d1418a16509bc7a0018e1259e53
gaschonfelder <gaschonfelder@gmail.com>
2025-06-04 21:39:38 -0300

    primeiro commit

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,14 @@
+// https://blockchain.info/ticker
+
+export default function initFetchbBitcoin() {
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((json) => {
+      const btcPreco = document.querySelector(".btc-preco");
+      btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
+      console.log(json.BRL.sell);
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
---
+++ js/script.js
@@ -0,0 +1,26 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown.js";
+import initMenuMobile from "./modules/menumobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchbBitcoin from "./modules/fetch-bitcoin.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchbBitcoin();
+
+import $ from "jquery";
+import _ from "lodash";
