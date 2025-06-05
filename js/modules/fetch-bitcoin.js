9beed5cab999644605dfacc89cceb5b5d0998976
gaschonfelder <gaschonfelder@gmail.com>
2025-06-04 21:50:45 -0300

    add packge-lock.jason

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -6,7 +6,7 @@ export default function initFetchbBitcoin() {
     .then((json) => {
       const btcPreco = document.querySelector(".btc-preco");
       btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
-      console.log(json.BRL.sell);
+      //console.log(json.BRL.sell);
     })
     .catch((erro) => {
       console.log(Error(erro));
